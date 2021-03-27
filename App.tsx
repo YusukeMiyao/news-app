18fb422eb2d718ad5bc398f71f198004cec6405d
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-03-27 21:11:08 +0900

    [add]style

--- App.tsx
+++ App.tsx
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderBottomColor:'gray',
+    borderWidth: 1,
+    flexDirection:'row'
+  },
+  leftContainer: {
+    width:100
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection:'column',
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer} >
+        <View style={styles.leftContainer}>
+          <Image style={{ width: 100, height: 100 }}
+            source={{ uri: 'https:facebook.github.io/react-native/img/tiny_logo.png' }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3}>
+            fadafjajfkjaljflajflkjslkjflaksjdfljasldfjkaslkjdflasdjflasjdlfjaslkdfjalsjfalsfjjffdsjfldsjflasfjjfadlsjfljaslkfjadsljflaksdjflkjadsdlfjkaslnfvaskjlkajfdlkjasldkjvalkjflasfa;l jfkjsfkjkfjlkdasjlfkadslfadsljflasjkdflasjflkajdslfkjlaksjflkajsflkjaslfkjlkdsfjd
+          </Text>
+          <Text>ReactNews</Text>
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
