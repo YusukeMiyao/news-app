f184fad81c0ea434d3366c660b76ed073086dd30
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-04-12 22:48:04 +0900

    section8_62

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
+import { ClipScreen } from '../screens/ClipScreen';
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => {
-
+const HomeStack = () => {
   return (
-    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
@@ -21,8 +23,23 @@ const AppNavigator = () => {
           component={ArticleScreen}
         />
       </Stack.Navigator>
+  )
+}
+
+const ClipStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Clip' component={ClipScreen} />
+    </Stack.Navigator>
+  )
+}
+export const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeStack}/>
+        <Tab.Screen name='Clip' component={ClipScreen} />
+    </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-export default AppNavigator;
--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
 import {StackNavigationProp} from "@react-navigation/stack";
 import {RootStackParamList} from "../types/navigation";
-import {RouteProp} from "@react-navigation/native";
+import { RouteProp } from "@react-navigation/native";
+import { useDispatch } from 'react-redux'
+import { addClip, deleteClip } from '../store/actions/user'
 
 const styles = StyleSheet.create({
   container: {
@@ -13,15 +15,26 @@ const styles = StyleSheet.create({
 })
 
 type Props = {
+  navigation: StackNavigationProp<RootStackParamList, "Article">;
   route: RouteProp<RootStackParamList, "Article">;
 }
 
 const ArticleScreen = ({ route }:Props) => {
-  const {article} = route.params
+  const { article } = route.params
+  
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => { dispatch(addClip({clip:article}))}}>
+        <Text style={{margin: 10, fontSize: 30}}>ADDDD</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => { dispatch(deleteClip({clip:article}))}}>
+        <Text style={{margin: 10, fontSize: 30}}>DELETE</Text>
+      </TouchableOpacity>
       <WebView source={{uri: article.url}} />
     </SafeAreaView>
+    
   )
 }
 
