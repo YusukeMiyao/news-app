a7cc56f89a813264612f376cf361a7f13b30ef7f
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-04-12 23:30:19 +0900

    section8_64

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 import { ClipScreen } from '../screens/ClipScreen';
+/* types */
+import {RootStackParamList} from "../types/navigation";
 
 
 const Stack = createStackNavigator();
@@ -33,10 +36,23 @@ const ClipStack = () => {
     </Stack.Navigator>
   )
 }
+
+const screenOption = ({ route }:any) => ({
+  tabBarIcon: ({ color, size }:any) => {
+    let iconName;
+
+    if (route.name === 'Home') {
+      iconName = 'home'
+    } else if (route.name === 'Clip') {
+      iconName = 'bookmark';
+    }
+    return <FontAwesome name={iconName} size={size} color={color} />
+  }
+})
 export const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenOption}>
         <Tab.Screen name='Home' component={HomeStack}/>
         <Tab.Screen name='Clip' component={ClipScreen} />
     </Tab.Navigator>
--- screens/ClipScreen.tsx
+++ screens/ClipScreen.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native'
+import {useSelector} from 'react-redux'
+import { StyleSheet, SafeAreaView, Text, FlatList } from 'react-native'
+import ListItem from '../components/ListItem';
+/* types */
+import {StackNavigationProp} from "@react-navigation/stack";
+import {RootStackParamList} from "../types/navigation";
+import {RouteProp} from "@react-navigation/native";
+import {State} from "../types/state";
+import {User} from "../types/user";
+
 
 const styles = StyleSheet.create({
   container: {
@@ -8,10 +17,28 @@ const styles = StyleSheet.create({
   }
 })
 
-export const ClipScreen = () => {
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, "Clip">;
+  route: RouteProp<RootStackParamList, "Clip">;
+};
+
+export const ClipScreen: React.FC<Props> = ({navigation, route}: Props) => {
+  const user = useSelector((state: State) => state.user) as User;
+  const { clips } = user;
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Clip Screen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={()=>navigation.navigate("Article", {article: item})}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   )
 }
